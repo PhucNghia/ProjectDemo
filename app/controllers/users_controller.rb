21b371b528909dd4acf8267a64fb162a7ee8b7d3
PhucNghia <ngocnghiaphuc@gmail.com>
2018-01-25 10:11:37 +0700

    Make users and posts

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,69 @@
+class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index, :new, :create]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+    @check_new = true
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "controller.users_controller.success_signup"
+      redirect_to user_params
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find_by_id params[:id]
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = "cap nhat thanh cong"
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "controller.users_controller.success_delete"
+      redirect_to users_path
+    else
+      flash[:danger] = t "controller.users_controller.cant_delete"
+      render :index
+    end
+  end
+
+  private
+
+  def load_user
+    @user = User.find_by_id params[:id]
+    return if @user
+    flash[:danger] = t "controller.users_controller.no_account"
+    redirect_to root_path
+  end
+
+  def user_params
+    params.require(:user).permit :name, :avatar, :email, :password, :password_confirmation,
+       :phone, :address
+  end
+
+  # def user_admin
+  #   unless current_user && current_user.admin?
+  #     redirect_to root_path
+  #   end
+  # end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  has_many :posts
+
+  devise :database_authenticatable, :registerable,
+   :recoverable, :rememberable, :trackable, :validatable
+
+  mount_uploader :avatar, PictureUploader
+  validates :name, length: {maximum: Settings.size_name}, presence: true
+  VALID_PHONE_REGEX = /[0-9\-\+\(\)]/
+  validates :phone, length: {in: Settings.min_phone..Settings.max_phone}, presence: true,
+    format: {with: VALID_PHONE_REGEX}
+  validates :address, length: {maximum: Settings.size_address}, presence: true
+
+  scope :info_user, ->{select :id, :name, :email, :phone, :address, :role}
+end
